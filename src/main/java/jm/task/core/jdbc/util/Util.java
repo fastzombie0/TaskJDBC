3e5e35a9761f058dd7c56adae4284852ae61617a
Kondrienko Andrey <zombiefast@yandex.ru>
2020-11-20 21:57:30 +0300

    1-commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    // реализуйте настройку соеденения с БДjdbc:mysql://Andrejs-MacBook-Pro.local:3306
+    Connection connection;
+
+    {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://Andrejs-MacBook-Pro.local:3306", "root", "09250925");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
